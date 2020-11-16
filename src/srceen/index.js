3d404cb12d6e90408a8a2f839f0a4d5e741db6d9
phong  nguyen <phongit1995@gmail.com>
2020-11-16 10:33:01 +0700

    fix android

--- src/srceen/index.js
+++ src/srceen/index.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import IconEvilIcons from 'react-native-vector-icons/EvilIcons';
+import IconFather from 'react-native-vector-icons/Feather';
 import * as SCREEN from './../constants/screen';
 import Home from './Home/index';
 import History from './History';
 import Category from './Category';
 import Follow from './Follow';
 import Info from './Info';
+import Login from './Login'
 const TabBottom = createBottomTabNavigator();
+const Stack = createStackNavigator();
 const Screen=()=>{
     return(
         <NavigationContainer >
-           <BottomTab></BottomTab>
+             <Stack.Navigator screenOptions={{headerShown:false}}>
+                <Stack.Screen name={SCREEN.MAIN_SCREEN} component={BottomTab} />
+                <Stack.Screen name={SCREEN.LOGIN_SCREEN} component={Login} />
+             </Stack.Navigator>
         </NavigationContainer>
     )
 }
 const BottomTab = ()=>{
     return (
-        <TabBottom.Navigator tabBarOptions={{style:{paddingBottom:5,borderRadius:10}}} initialRouteName={SCREEN.HOME_SCREEN}>
+        <TabBottom.Navigator tabBarOptions={{style:{paddingBottom:5,borderRadius:10}}} initialRouteName={SCREEN.HOME_SCREEN} >
             <TabBottom.Screen 
                 name={SCREEN.HOME_SCREEN} 
                 component={Home}
-                options={{ title: 'Trang Chu' ,
+                options={{ title: 'Trang Chủ',
                 tabBarIcon:({focused})=>{
                     if(focused){
                         return <Icon name={"home"} size={30}/>
@@ -35,52 +44,52 @@ const BottomTab = ()=>{
             <TabBottom.Screen 
                 name={SCREEN.CATEGORY_SCREEN} 
                 component={Category}
-                options={{ title: 'The Loai' ,
+                options={{ title: 'Thể Loại' ,
                 tabBarIcon:({focused})=>{
                     if(focused){
-                        return <Icon name={"home"} size={30}/>
+                        return <IconFather name={"layers"} size={30}/>
                     }
                     else {
-                        return <Icon name={"home"} size={20}/>
+                        return <IconFather name={"layers"} size={20}/>
                     }
                 }}}
             />
             <TabBottom.Screen 
                 name={SCREEN.FOLLOW_SCREEN} 
                 component={Follow}
-                options={{ title: 'Theo Doi' ,
+                options={{ title: 'Theo Dõi' ,
                 tabBarIcon:({focused})=>{
                     if(focused){
-                        return <Icon name={"home"} size={30}/>
+                        return <Icon name={"wifi"} size={30}/>
                     }
                     else {
-                        return <Icon name={"home"} size={20}/>
+                        return <Icon name={"wifi"} size={20}/>
                     }
                 }}}
             />
             <TabBottom.Screen 
                 name={SCREEN.HISTORY_SCREEN} 
                 component={History}
-                options={{ title: 'Lich Su',
+                options={{ title: 'Lịch Sử',
                 tabBarIcon:({focused})=>{
                     if(focused){
-                        return <Icon name={"home"} size={30}/>
+                        return <FontAwesome name={"calendar"} size={30}/>
                     }
                     else {
-                        return <Icon name={"home"} size={20}/>
+                        return <FontAwesome name={"calendar"} size={20}/>
                     } 
                 }}}
             />
             <TabBottom.Screen 
                 name={SCREEN.INFO_SCREEN} 
                 component={Info}
-                options={{ title: 'Toi' ,
+                options={{ title: 'Tôi' ,
                 tabBarIcon:({focused})=>{
                     if(focused){
-                        return <Icon name={"home"} size={30}/>
+                        return <IconEvilIcons name={"user"} size={30}/>
                     }
                     else {
-                        return <Icon name={"home"} size={20}/>
+                        return <IconEvilIcons name={"user"} size={20}/>
                     } 
                 }}} 
             />
