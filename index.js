0a19430568e8aeae1a06fc5c7d101d7f30f8f8e1
phongit <phongit1995@gmail.com>
2020-11-22 18:06:14 +0700

    add fire base

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 
 import 'react-native-get-random-values'
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
-AppRegistry.registerComponent(appName, () => App);
+import messaging from '@react-native-firebase/messaging';
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+});
+function HeadlessCheck({ isHeadless }) {
+    if (isHeadless) {
+      // App has been launched in the background by iOS, ignore
+      return null;
+    }
+    return <App />;
+  }
+AppRegistry.registerComponent(appName, () => HeadlessCheck);
