8e4a86df8669cfae4270246be19a6731a1b5535a
phong  nguyen <phongit1995@gmail.com>
2020-12-16 17:34:27 +0700

    add name

--- src/srceen/DetialChapter/index.js
+++ src/srceen/DetialChapter/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image, Dimensions, ActivityIndicator ,Animated ,FlatList } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image, Dimensions, ActivityIndicator ,Animated ,FlatList ,StatusBar } from 'react-native';
 import Entypo from 'react-native-vector-icons/Entypo';
 import { useNavigation } from '@react-navigation/native';
 const { height, width } = Dimensions.get("window");
@@ -7,6 +7,7 @@ import { getDetailChapter } from './../../api/comic'
 import {makeUserName} from './../../common/stringHelper';
 import AdmodService from "./../../firebase/Admod";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome'
 
 export default function DetialComic({ route }) {
     const { id } = route.params
@@ -14,16 +15,27 @@ export default function DetialComic({ route }) {
     const [name, setName] = useState("Chi Tiết");
     const [imagesList, setImagesList] = useState([])
     const [isLoading, setIsLoading] = useState(true);
-    const scrollY = new Animated.Value(0)
-    const diffClamp = Animated.diffClamp(scrollY,0,height/12)
+    const [beforeChapter,setBeforeChapter]=useState(null);
+    const [afterChapter,setAfterChapter]=useState(null);
+    const scrollY = new Animated.Value(0);
+    const scrollYFooter = new Animated.Value(0);
+    const diffClamp = Animated.diffClamp(scrollY,0,height / 9.5)
     const translateY = diffClamp.interpolate({
-        inputRange:[0,height/12],
-        outputRange:[0,-(height/12)]
+        inputRange:[0,height / 9.5],
+        outputRange:[0,-(height / 9.5)]
+    })
+    const diffClampFooter = Animated.diffClamp(scrollYFooter,0,height / 12)
+    const translateYFooter = diffClampFooter.interpolate({
+        inputRange:[0,height / 12],
+        outputRange:[0,height / 12]
     })
     useEffect(() => {
         getDetailChapter(id).then((resultData)=>{
             if (resultData?.data?.status == "success") {
-                setName("Chương : " + resultData.data.data.index)
+                setName("Chương : " + resultData.data.data.index);
+                console.log(resultData.data?.data);
+                setAfterChapter(resultData.data?.data?.after);
+                setBeforeChapter(resultData.data?.data?.before);
                 setImagesList(resultData.data?.data?.images);
                 setIsLoading(false)
             }
@@ -48,12 +60,13 @@ export default function DetialComic({ route }) {
     } else {
         return (
             <View style={styles.container}>
+                <StatusBar  translucent backgroundColor="transparent" />
                 <Animated.View style={[styles.Header,{
                     transform:[
                         {translateY:translateY}
                     ]
                 }]}>
-                    <TouchableOpacity onPress={() => navigation.goBack()} >
+                    <TouchableOpacity onPress={() => {navigation.goBack();console.log("Phong")}}>
                         <Entypo name="chevron-thin-left" color="#fff" size={20} style={{ paddingLeft: 5 }} />
                     </TouchableOpacity>
                     <Text style={styles.name}>{name}</Text>
@@ -76,10 +89,35 @@ export default function DetialComic({ route }) {
                     keyExtractor={(item,index) =>item+index}
                     renderItem={({item})=><ImageFullWith url={item}/>}
                     onScroll={(e)=>{
-                        scrollY.setValue(e.nativeEvent.contentOffset.y) 
+                        scrollY.setValue(e.nativeEvent.contentOffset.y);
+                        scrollYFooter.setValue(e.nativeEvent.contentOffset.y)
                     }}
                     />
                 </View>
+                <Animated.View style={[styles.Footer,{
+                    transform:[
+                        {translateY:translateYFooter}
+                    ]
+                }]}>
+                    <View style={{flex:1,flexDirection:"row",alignItems:"center",justifyContent:"space-between"}}>
+                        <View >
+                            {beforeChapter!=null?
+                                <TouchableOpacity onPress={()=>{console.log(beforeChapter)}} style={{flexDirection:"row",justifyContent:"center",alignItems:"center"}}>
+                                    <FontAwesome5 name={"angle-left"} size={35} color={"#ffffff"}/>
+                                    <Text style={[{marginLeft:7},styles.textChapter]}>Chương Trước</Text>
+                                </TouchableOpacity>:null
+                            }
+                        </View>
+                        <View >
+                            {afterChapter!=null?
+                                <TouchableOpacity style={{flexDirection:"row",justifyContent:"center",alignItems:"center"}}>
+                                    <Text style={[{marginRight:7},styles.textChapter]}>Chương Sau</Text>
+                                <FontAwesome5 name={"angle-right"} size={35} color={"#ffffff"}/>
+                                </TouchableOpacity>:null
+                            }
+                        </View>
+                    </View>
+                </Animated.View>
             </View>
         );
     }
@@ -117,8 +155,7 @@ const  ShowAdsChapter=async()=>{
 }
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-
+        flex: 1
     },
     containers: {
         flex: 1,
@@ -140,12 +177,13 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         paddingVertical: 10,
         justifyContent: "center",
-        height: height / 12,
+        height: height / 9.5,
         alignItems: 'center',
         borderBottomWidth: 1,
-        borderBottomColor: '#d90429',
-        backgroundColor: '#e63946',
-       
+        borderBottomColor: '#404042',
+        backgroundColor: '#404042',
+        paddingTop:30,
+        zIndex:10
     },
     name: {
         textTransform: 'uppercase',
@@ -175,4 +213,21 @@ const styles = StyleSheet.create({
     endchap: {
         textAlign: 'center'
     },
+    Footer:{
+        position:"absolute",
+        bottom:0,
+        left:0,
+        height:height/13,
+        paddingHorizontal:10,
+        elevation:6,
+        width:width,
+        backgroundColor: '#404042',
+        justifyContent: "center",
+        opacity:0.8,
+        zIndex:10
+    },
+    textChapter:{
+        fontSize:14,
+        color:"#b8b4b4"
+    }
 })
